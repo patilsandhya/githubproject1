a65ba12b0b9458e8611d322e3fe89a9f5dfa9b5e
AshaJadhav <jadhavasha036@gmail.com>
2023-06-06 16:42:19 +0530

    hlo

---
+++ src/githubpriject1/Test.java
@@ -0,0 +1,14 @@
+package githubpriject1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hlo");
+		int i=10;
+		String name="sandhya";
+		System.out.println(i);
+		System.out.println(name);
+
+	}
+
+}
